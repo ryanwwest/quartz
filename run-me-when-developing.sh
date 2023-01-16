b078ed4bc9c1c037a32660ef5127dd580a4f1e91
ryanwwest <ryan@ryanwwest.com>
2023-01-16 06:20:16 +0000

    auto-find and pull in images referenced in blog entries markdown; create script to do this and place in content/media; modify textprocessing.html partial to recognize image files as being always in the media folder
    Signed-off-by: ryanwwest <ryan@ryanwwest.com>

--- run-me-when-developing.sh
+++ run-me-when-developing.sh
@@ -1,1 +1,4 @@
-watch -n 3 bash -c 'hugo-obsidian -input=content -output=assets/indices -index=true -root=. && hugo'
+# python script fetches markdown and images from obsidian vault, places in content/
+# hugo-obsidian creates tags and taxonomies and links in json for quartz to use
+# hugo builds static html site in public/ using both of the above
+watch -n 5 bash -c 'python3 obsidian-to-quartz.py ~/g/zk/media/ ~/g/zk/blog/ ~/g/quartz/content/media/ ~/g/quartz/content/ && hugo-obsidian -input=content -output=assets/indices -index=true -root=. && hugo'
